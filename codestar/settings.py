bc2f0bd2afbbf0e3a7d6d881b8793498c2f4a28a
Nicola Getsee <Nicola.getsee@gmail.com>
2024-08-03 16:40:20 +0000

    tidy code

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 from pathlib import Path
@@ -143,6 +144,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+MESSAGE_TAGS = {
+    messages.SUCCESS:'alert-success',
+    messages.ERROR:'alert-danger',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
